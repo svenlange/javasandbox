eaa3a9196b39192a7bb3f42faa45aba3053b3221
svenlange <sv3n.lange@gmail.com>
2013-03-09 14:36:36 +0100

    speedup rendering by exchanging layout gridpane with canvans.

--- src/za/co/svenlange/gameoflife/ui/UserInterface.java
+++ src/za/co/svenlange/gameoflife/ui/UserInterface.java
@@ -3,80 +3,79 @@ package za.co.svenlange.gameoflife.ui;
 import javafx.application.Application;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.GridPaneBuilder;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.RectangleBuilder;
 import javafx.stage.Stage;
 import za.co.svenlange.gameoflife.Grid;
 import za.co.svenlange.gameoflife.PredefinedGrids;
 
 public class UserInterface extends Application {
 
+    private static final int CELL_SIZE = 18;
+    private static final int BORDER_WIDTH = 1;
+    private static final int OFFSET = 10;
+    private static final Color BACKGROUND_COLOR = Color.DARKGRAY;
+    private static final Color GRID_COLOR = Color.BLACK;
+    private static final Color CELL_COLOR = Color.BLUE;
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Conway's Game of Life");
-
-        final GridPane gridPane = GridPaneBuilder.create()
-                .padding(new Insets(4))
-                .gridLinesVisible(true)
-                .focusTraversable(true)
-                .build();
+        stage.setTitle("Conway's Game of Life by Sven Lange");
 
         Grid grid = PredefinedGrids.getOctagon2Grid();
 
-        populateGridPane(gridPane, grid);
-
-        stage.setScene(new Scene(gridPane));
+        final Group root = new Group();
+        root.getChildren().add(createCanvas(grid));
+        stage.setScene(new Scene(root, sceneSize(grid), sceneSize(grid), BACKGROUND_COLOR));
         stage.show();
 
-
         final GridService service = new GridService(grid);
-        service.start();
-
         service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
             @Override
             public void handle(WorkerStateEvent workerStateEvent) {
                 Grid grid = (Grid) workerStateEvent.getSource().getValue();
-                populateGridPane(gridPane, grid);
+                root.getChildren().clear();
+                root.getChildren().add(createCanvas(grid));
                 service.restart();
             }
         });
-
+        service.start();
     }
 
-    private void populateGridPane(GridPane gridPane, Grid grid) {
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid.size(); j++) {
+    private int sceneSize(Grid grid) {
+        return grid.size() * CELL_SIZE + OFFSET * 2;
+    }
 
-                Color color = getColor(grid.isCellAlive(i, j));
-
-                Rectangle rectangle = RectangleBuilder.create()
-                        .height(15)
-                        .width(15)
-                        .fill(color)
-                        .build();
+    private Canvas createCanvas(Grid grid) {
+        Canvas canvas = new Canvas(sceneSize(grid), sceneSize(grid));
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
 
-                gridPane.add(rectangle, i, j);
+        for (int x = 0; x < grid.size(); x++) {
+            for (int y = 0; y < grid.size(); y++) {
+                gc.setFill(GRID_COLOR);
+                gc.fillRect(x * CELL_SIZE + OFFSET, y * CELL_SIZE + OFFSET, CELL_SIZE, BORDER_WIDTH);
+                gc.fillRect(x * CELL_SIZE + OFFSET, y * CELL_SIZE + OFFSET, BORDER_WIDTH, CELL_SIZE);
+
+                if (grid.isCellAlive(x, y)) {
+                    gc.setFill(CELL_COLOR);
+                    gc.fillRect(x * CELL_SIZE + BORDER_WIDTH + OFFSET, y * CELL_SIZE + BORDER_WIDTH + OFFSET, CELL_SIZE, CELL_SIZE);
+                }
             }
         }
-    }
 
-    private Color getColor(boolean cellAlive) {
-        if (cellAlive) {
-            return Color.BROWN;
-        } else {
-            return Color.ANTIQUEWHITE;
-        }
+        int size = grid.size() * CELL_SIZE;
+        gc.setFill(GRID_COLOR);
+        gc.fillRect(size + OFFSET, OFFSET, BORDER_WIDTH, size + BORDER_WIDTH);
+        gc.fillRect(OFFSET, size + OFFSET, size, BORDER_WIDTH);
+
+        return canvas;
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
-
 }
