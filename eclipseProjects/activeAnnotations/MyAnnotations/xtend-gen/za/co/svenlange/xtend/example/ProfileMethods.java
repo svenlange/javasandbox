946673b486a75b57bd38f471aa5826a6c50458df
Sven Lange <sv3n.lange@gmail.com>
2014-12-26 00:12:03 +0100

    removed xtend-gen folders from activeAnnotations project

--- eclipseProjects/activeAnnotations/MyAnnotations/xtend-gen/za/co/svenlange/xtend/example/ProfileMethods.java
+++
@@ -1,8 +0,0 @@
-package za.co.svenlange.xtend.example;
-
-import org.eclipse.xtend.lib.macro.Active;
-import za.co.svenlange.xtend.example.ProfileMethodsClassProcessor;
-
-@Active(ProfileMethodsClassProcessor.class)
-public @interface ProfileMethods {
-}
